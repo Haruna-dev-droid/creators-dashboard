742a171c986308fa3e8da4571b826681aa714285
John Doe <samailah40@gmail.com>
2026-02-19 14:03:20 +0100

    worked on the key down function on the todo input filed

--- src/components/pages/Todo.jsx
+++ src/components/pages/Todo.jsx
@@ -55,6 +55,7 @@ function Todo() {
           type="text"
           value={input}
           onChange={(e) => setInput(e.target.value)}
+          onKeyDown={(e) => e.key === "Enter" && addTodo()}
           className="border border-gray-300 rounded-l-lg px-4 py-2 w-full focus:outline-none focus:ring-2 focus:ring-blue-500"
           placeholder="Add a new todo..."
         />
